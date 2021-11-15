cc473783dffccf7d9a040ed5bc1007a459b2c1e0
SindreH <sindre.hepsoe@gmail.com>
2021-11-15 21:31:30 +0100

    some more jsx

--- src/App.js
+++ src/App.js
@@ -5,21 +5,14 @@ import {
 } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <Routes>
       
           <Route exact path="/" element={<HomePage />} />
-          <Route path="/hats" element={<HatsPage />} />
+          <Route path="/shop" element={<ShopPage />} />
       
     </Routes>
   );
