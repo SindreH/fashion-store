45bf15e32f5ad212ce23eb05784491f1548a153c
SindreH <sindre.hepsoe@gmail.com>
2021-11-16 02:57:09 +0100

    more frontend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -16,6 +17,7 @@ function App() {
         
             <Route exact path="/" element={<HomePage />} />
             <Route path="/shop" element={<ShopPage />} />
+            <Route path="/signin" element={<SignInAndSignUp />}/>
         
       </Routes>
     </div>
