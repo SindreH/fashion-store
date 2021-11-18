dd6b0e61d8b3ad89bcffb96db29e3a1422e55dcb
SindreH <sindre.hepsoe@gmail.com>
2021-11-18 17:11:09 +0100

    firebase login

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -20,13 +20,26 @@ class App extends React.Component {
   }
 
   unsubscribedFromAuth = null
-
+ 
   componentDidMount() {
-    this.unsubscribedFromAuth  = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user
-      })
-      console.log(user)
+    this.unsubscribedFromAuth  = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id, ...snapShot.data()
+              }
+            }) 
+            console.log(this.state)
+        })
+        
+      } else {
+        this.setState({
+          currentUser: userAuth
+        })
+      }
     })
   }
 
